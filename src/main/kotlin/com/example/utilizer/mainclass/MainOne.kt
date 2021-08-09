bce3318356e7615122632787666ee403653cc6ff
ravipandhi <rpandhi2018@my.fit.edu>
2021-08-09 13:07:11 -0400

    initial

---
+++ src/main/kotlin/com/example/utilizer/mainclass/MainOne.kt
@@ -0,0 +1,7 @@
+package com.example.utilizer.mainclass
+
+class MainOne {
+    fun mainFunction() {
+        System.out.println("start of main function")
+    }
+}
---
+++ src/test/kotlin/com/example/utilizer/UtilizerApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.utilizer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UtilizerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
